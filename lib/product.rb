529da826ee3f8114cbdcf7056a476cfccce932ac
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-10-05 18:35:52 +0200

    add require product to love

--- lib/product.rb
+++ lib/product.rb
@@ -10,11 +10,11 @@ class Product
   property :sugar_content_gram, Float
   property :ranking, Integer
 
-  validates_presence_of :brand, message: "Please fill in brand."
-  validates_presence_of :product_name, message: "Please fill in a name."
-  validates_presence_of :category, message: "Please fill in a category."
-  validates_presence_of :barcode, message: "Please fill in barcode."
-  validates_uniqueness_of :barcode, message: "Barcode already exists."
-  validates_presence_of :sugar_content_gram, message: "Please fill in sugar content."
+   validates_presence_of :brand, message: "Please fill in brand."
+   validates_presence_of :product_name, message: "Please fill in a name."
+   validates_presence_of :category, message: "Please fill in a category."
+   validates_presence_of :barcode, message: "Please fill in barcode."
+   validates_uniqueness_of :barcode, message: "Barcode already exists."
+   validates_presence_of :sugar_content_gram, message: "Please fill in sugar content."
 
 end
